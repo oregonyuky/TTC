eb5d452ccfbbbf01f138fd55395e941629a18377
oregonyuky <felipe.akio.nishimura@gmail.com>
2026-09-17 13:34:57 -0300

    Acabado

--- Filtros.cs
+++ Filtros.cs
@@ -914,35 +914,47 @@ namespace ProcessamentoImagens
 
         public unsafe static byte* p0(int i, int j, BitmapData bd)
         {
-            return (byte*)(i * bd.Stride) + ((j + 1) * pixelSize);
+            return (j < bd.Width - 1) ? (byte*)(i * bd.Stride) + ((j + 1) * pixelSize) : null;
         }
+
         public unsafe static byte* p1(int i, int j, BitmapData bd)
         {
-            return (byte*)((i-1) * bd.Stride) + ((j + 1) * pixelSize);
+            return (i > 0 && j < bd.Width - 1) ? (byte*)((i - 1) * bd.Stride) + ((j + 1) * pixelSize) : null;
         }
+
         public unsafe static byte* p2(int i, int j, BitmapData bd)
         {
-            return (byte*)((i-1) * bd.Stride) + (j * pixelSize);
+            return (i > 0) ? (byte*)((i - 1) * bd.Stride) + (j * pixelSize) : null;
         }
+
         public unsafe static byte* p3(int i, int j, BitmapData bd)
         {
-            return (byte*)((i-1) * bd.Stride) + ((j-1) * pixelSize);
+            return (i > 0 && j > 0) ? (byte*)((i - 1) * bd.Stride) + ((j - 1) * pixelSize) : null;
         }
+
         public unsafe static byte* p4(int i, int j, BitmapData bd)
         {
-            return (byte*)(i * bd.Stride) + ((j-1) * pixelSize);
+            return (j > 0) ? (byte*)(i * bd.Stride) + ((j - 1) * pixelSize) : null;
         }
+
         public unsafe static byte* p5(int i, int j, BitmapData bd)
         {
-            return (byte*)((i+1) * bd.Stride) + ((j-1) * pixelSize);
+            return (i < bd.Height - 1 && j > 0) ? (byte*)((i + 1) * bd.Stride) + ((j - 1) * pixelSize) : null;
         }
+
         public unsafe static byte* p6(int i, int j, BitmapData bd)
         {
-            return (byte*)((i+1) * bd.Stride) + (j * pixelSize);
+            return (i < bd.Height - 1) ? (byte*)((i + 1) * bd.Stride) + (j * pixelSize) : null;
         }
+
         public unsafe static byte* p7(int i, int j, BitmapData bd)
         {
-            return (byte*)((i+1) * bd.Stride) + ((j+1) * pixelSize);
+            return (i < bd.Height - 1 && j < bd.Width - 1) ? (byte*)((i + 1) * bd.Stride) + ((j + 1) * pixelSize) : null;
+        }
+
+        public unsafe static byte* getP(int i, int j, BitmapData bd)
+        {
+            return (i >= 0 && i < bd.Height && j >= 0 && j < bd.Width) ? (byte*)(i * bd.Stride) + (j * pixelSize) : null;
         }
 
         public unsafe static void binarizarEntrada(BitmapData bd)
@@ -981,6 +993,82 @@ namespace ProcessamentoImagens
                 }
             }
         }
+
+        private static unsafe bool ehBranco(byte* pixel)
+        {
+            return pixel[0] == 255 && pixel[1] == 255 && pixel[2] == 255;
+        }
+        
+        private static unsafe bool marcarPixel(byte* pixel)
+        {
+            return pixel[0] = 255 && pixel[1] =0 && pixel[2] = 0;
+        }
+
+        public static ehConectividade4(x1, y1, x2, y2){
+            return Math.Abs(x1 - x2) + Math.Abs(y1 - y2)==1;
+        }
+        public static void acharPos(int i, int j, int direcao, BitmapData bd, List<int> dir, Pilha p){
+            byte*[] pixelsVet = new vet*[8] {p0(i,j,bd),p1(i,j,bd),p2(i,j,bd),p3(i,j,bd),p4(i,j,bd),p5(i,j,bd),p6(i,j,bd),p7(i,j,bd)};
+            byte* pixelAtual = (byte*)bd.Scan0.ToPointer() + i*bd.Stride + j*pixelSize;
+            for(int t=1;t<=8;t++){
+                int idx = (direcao + t)%8;
+                if(pixelsVet[idx] != null && ehPreto(pixelsVet[idx])){
+                    int idxAnt = (idx+7)%8;
+                    if(pixelsVet[idxAnt]!=null && ehBrancho(pixelsVet[idxAnt])){
+                        int x, y;
+                        switch (idxAnt)
+                        {
+                            case 0: x = j + 1; y = i; break; // direita
+                            case 1: x = j + 1; y = i - 1; break; // acima direita
+                            case 2: x = j; y = i - 1; break; // acima
+                            case 3: x = j - 1; y = i - 1; break; // acima esquerda
+                            case 4: x = j - 1; y = i; break; // esquerda
+                            case 5: x = j - 1; y = i + 1; break; // abaixo esquerda
+                            case 6: x = j; y = i + 1; break; // abaixo
+                            case 7: x = j + 1; y = i + 1; break; // abaixo direita
+                        }
+                        if(ehConectividade4(x, y, i, j)){
+                            dir.Insert(0, (idxAnt+4)%8);
+                            p.push(new Info(x, y));
+                        } else {
+                            int posAnt = (idx+7)%8;
+                            int posProx = (idx+1)%8;
+                            if((pixelsVet[posAnt]!=null && ehBranco(pixelsVet[posAnt])) || (pixelsVet[posProx]!=null && ehBranco(pixelsVet[posProx]))){
+                                dir.Insert(0, (idxAnt+4)%8);
+                                p.push(new Info(x, y));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public static boolean marcado(byte* aux){
+            return aux[0]==255 && aux[1]==0 && aux[2]==0;
+        }
+        public unsafe static void escreverMarcadosNaImagemDestino(BitmapData bmS, BitmapData bmD){
+            int width = bmD.Width;
+            int height = bmD.Height;
+            int padding = bmS.Stride - width*pixelSize;
+            byte* src = (byte*)bmS.Scan0.ToPointer();
+            byte* dst = (byte*)bmD.Scan0.ToPointer();
+            for(int i=0;i<height;i++){
+                for(int j=0;j<width;j++){
+                    if(marcado(src)){
+                        *(dst++) = 0;
+                        *(dst++) = 0;
+                        *(dst++) = 0;
+                    } else {
+                        *(dst++) = 255;
+                        *(dst++) = 255;
+                        *(dst++) = 255;
+                    }
+                    src += 3;
+                }
+                src += padding;
+                dst += padding;
+            }
+        }
         public static void contourFollowingDMA(Bitmap imageBitmapSrc, Bitmap imageBitmapDest)
         {
             int width = imageBitmapSrc.Width;
@@ -988,11 +1076,11 @@ namespace ProcessamentoImagens
             BitmapData bmS = imageBitmapSrc.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
             BitmapData bmD = imageBitmapDest.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
             int padding = bmS.Stride - (width * pixelSize);
-            List<int> direcoes = new List<int>();
+            List<int> dir = new List<int>();
             Pilha p = new Pilha();
             unsafe
             {
-                byte* src = (byte*) bmS.Scan0.ToPointer();
+                byte* src = (byte*)bmS.Scan0.ToPointer();
                 byte* dst = (byte*)bmD.Scan0.ToPointer();
                 binarizarEntrada(bmS);
                 branquearSaida(bmD);
@@ -1001,9 +1089,27 @@ namespace ProcessamentoImagens
                     for(int j = 0; j < width; j++)
                     {
                         byte* pixelAtual = src + i * bmS.Stride + j * pixelSize;
+                        byte* pixelDireita = getP(i, j+1, bmS);
+                        if(ehBranco(pixelAtual) && pixelDireita!=null && ehPreto(pixelDireita)){
+                            byte* destino = pixelAtual;
+                            byte* atualContorno = inicial;
+                            int iAtual = i, jAtual = j, direcao=4;
+                            do{
+                                marcarPixel(atualContorno);
+                                acharPos(iAtual, jAtual, direcao, bmS, dir, p);
+                                if(!p.isEmpty()){
+                                    int direcao = dir[0];
+                                    Info info = p.pop();
+                                    atualContorno = getP(info.getY(), info.getX(), bd);
+                                }
+                            }while(atualContorno != destino);
+                        }
                     }
                 }
+                escreverMarcadosNaImagemDestino(bmS, bmD);
             }
+            imageBitmapSrc.UnlockBits(bmS);
+            imageBimapDest.UnlockBits(bmD);
         }
     }
 }
